5b6d9963a82d0fd169bd0c09f78bb7916a2a01e3
Marco Carvalho <marcolucio27@gmail.com>
2023-07-11 23:58:42 -0300

    Update HelmetMiddleware.cs (#5)

--- src/HelmetMiddleware.cs
+++ src/HelmetMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace Helmet;
@@ -16,117 +18,33 @@ public class HelmetMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        if (!_options.UseContentSecurityPolicy)
+        var optionsMapping = new Dictionary<Func<bool>, (string HeaderKey, string HeaderValue)>
         {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.ContentSecurityPolicy);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.ContentSecurityPolicy] = HttpHeaderValueConstants.ContentSecurityPolicy;
-        }
+            { () => _options.UseContentSecurityPolicy, (HttpHeaderKeyConstants.ContentSecurityPolicy, HttpHeaderValueConstants.ContentSecurityPolicy) },
+            { () => _options.UseCrossOriginOpenerPolicy, (HttpHeaderKeyConstants.CrossOriginOpenerPolicy, HttpHeaderValueConstants.CrossOriginOpenerPolicy) },
+            { () => _options.UseCrossOriginResourcePolicy, (HttpHeaderKeyConstants.CrossOriginResourcePolicy, HttpHeaderValueConstants.CrossOriginResourcePolicy) },
+            { () => _options.UseOriginAgentCluster, (HttpHeaderKeyConstants.OriginAgentCluster, HttpHeaderValueConstants.OriginAgentCluster) },
+            { () => _options.UseReferrerPolicy, (HttpHeaderKeyConstants.ReferrerPolicy, HttpHeaderValueConstants.ReferrerPolicy) },
+            { () => _options.UseStrictTransportSecurity, (HttpHeaderKeyConstants.StrictTransportSecurity, HttpHeaderValueConstants.StrictTransportSecurity) },
+            { () => _options.UseXContentTypeOptions, (HttpHeaderKeyConstants.XContentTypeOptions, HttpHeaderValueConstants.XContentTypeOptions) },
+            { () => _options.UseDnsPrefetchControl, (HttpHeaderKeyConstants.DnsPrefetchControl, HttpHeaderValueConstants.DnsPrefetchControl) },
+            { () => _options.UseDownloadOptions, (HttpHeaderKeyConstants.DownloadOptions, HttpHeaderValueConstants.DownloadOptions) },
+            { () => _options.UseFrameOptions, (HttpHeaderKeyConstants.FrameOptions, HttpHeaderValueConstants.FrameOptions) },
+            { () => _options.UsePermittedCrossDomainPolicies, (HttpHeaderKeyConstants.PermittedCrossDomainPolicies, HttpHeaderValueConstants.PermittedCrossDomainPolicies) },
+            { () => _options.UseXPoweredBy, (HttpHeaderKeyConstants.XPoweredBy, null) },
+            { () => _options.UseXXssProtection, (HttpHeaderKeyConstants.XXssProtection, HttpHeaderValueConstants.XXssProtection) },
+        };
 
-        if (!_options.UseCrossOriginOpenerPolicy)
+        foreach (var option in optionsMapping)
         {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.CrossOriginOpenerPolicy);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.CrossOriginOpenerPolicy] = HttpHeaderValueConstants.CrossOriginOpenerPolicy;
-        }
-
-        if (!_options.UseCrossOriginResourcePolicy)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.CrossOriginResourcePolicy);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.CrossOriginResourcePolicy] = HttpHeaderValueConstants.CrossOriginResourcePolicy;
-        }
-
-        if (!_options.UseOriginAgentCluster)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.OriginAgentCluster);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.OriginAgentCluster] = HttpHeaderValueConstants.OriginAgentCluster;
-        }
-
-        if (!_options.UseReferrerPolicy)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.ReferrerPolicy);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.ReferrerPolicy] = HttpHeaderValueConstants.ReferrerPolicy;
-        }
-
-        if (!_options.UseStrictTransportSecurity)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.StrictTransportSecurity);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.StrictTransportSecurity] = HttpHeaderValueConstants.StrictTransportSecurity;
-        }
-
-        if (!_options.UseXContentTypeOptions)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.XContentTypeOptions);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.XContentTypeOptions] = HttpHeaderValueConstants.XContentTypeOptions;
-        }
-
-        if (!_options.UseDnsPrefetchControl)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.DnsPrefetchControl);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.DnsPrefetchControl] = HttpHeaderValueConstants.DnsPrefetchControl;
-        }
-
-        if (!_options.UseDownloadOptions)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.DownloadOptions);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.DownloadOptions] = HttpHeaderValueConstants.DownloadOptions;
-        }
-
-        if (!_options.UseFrameOptions)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.FrameOptions);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.FrameOptions] = HttpHeaderValueConstants.FrameOptions;
-        }
-
-        if (!_options.UsePermittedCrossDomainPolicies)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.PermittedCrossDomainPolicies);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.PermittedCrossDomainPolicies] = HttpHeaderValueConstants.PermittedCrossDomainPolicies;
-        }
-
-        if (!_options.UseXPoweredBy)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.XPoweredBy);
-        }
-
-        if (!_options.UseXXssProtection)
-        {
-            context.Response.Headers.Remove(HttpHeaderKeyConstants.XXssProtection);
-        }
-        else
-        {
-            context.Response.Headers[HttpHeaderKeyConstants.XXssProtection] = HttpHeaderValueConstants.XXssProtection;
+            if (!option.Key())
+            {
+                context.Response.Headers.Remove(option.Value.HeaderKey);
+            }
+            else if (option.Value.HeaderValue != null)
+            {
+                context.Response.Headers[option.Value.HeaderKey] = option.Value.HeaderValue;
+            }
         }
 
         await _next(context);
